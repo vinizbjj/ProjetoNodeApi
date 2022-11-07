6bc240d5ba06ada257cba93ba813e6950135f2d7
vinizbjj <viniz.bjj@gmail.com>
2022-11-07 10:50:01 -0300

    Create Tags Controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const usersRouter = require("./user.routes");
 const notesRouter = require("./movie_notes.routes");
+const tagsRouter = require("./tags_notes.routes")
 
 const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
